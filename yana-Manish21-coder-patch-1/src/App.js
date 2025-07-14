a401049386f4be64145ad1fcd484d83575aee068
Manish Kumar <raghavmanishprakash@gmail.com>
2025-07-14 20:26:06 +0530

    v_1.0

---
+++ yana-Manish21-coder-patch-1/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import ScrollToTop from "./components/ScrollToTop"; 
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import FAQ from './pages/FAQ';
+import Book from './pages/Book';
+import Blog from './pages/Blog';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/book" element={<Book />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
+        </main>
+        <Footer />
+        
+      </div>
+    </Router>
+  );
+}
+
+
+
+export default App;
